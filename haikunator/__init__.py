50bbdca82b799166e46a4c52d94ec1e0b55fb242
Atrox <mail@atrox.me>
2015-03-26 10:34:34 +0100

    words updated
    Signed-off-by: Atrox <mail@atrox.me>

--- haikunator/__init__.py
+++ haikunator/__init__.py
@@ -11,45 +11,40 @@ def haikunate(delimiter='-', tokenLength=4, tokenHex=False, tokenChars='01234567
     :param tokenChars:
     :return: string
     """
-    adjs = ['afternoon', 'aged', 'ancient', 'autumn', 'billowing',
-            'bitter', 'black', 'blue', 'bold', 'broken',
-            'calm', 'caring', 'cold', 'cool', 'crimson',
-            'damp', 'dark', 'dawn', 'delicate', 'divine',
-            'dry', 'empty', 'ephemeral', 'evening', 'falling',
-            'fathomless', 'floral', 'fragrant', 'frosty', 'golden',
-            'green', 'hidden', 'holy', 'icy', 'imperfect',
-            'impermanent', 'late', 'lingering', 'little', 'lively',
-            'long', 'majestic', 'mindful', 'misty', 'morning',
-            'muddy', 'nameless', 'noble', 'old', 'patient',
-            'polished', 'proud', 'purple', 'quiet', 'red',
-            'restless', 'rough', 'shy', 'silent', 'silvery',
-            'slender', 'small', 'smooth', 'snowy', 'solitary',
-            'sparkling', 'spring', 'stately', 'still', 'strong',
-            'summer', 'timeless', 'twilight', 'unknowable', 'unmovable',
-            'upright', 'wandering', 'weathered', 'white', 'wild',
-            'winter', 'wispy', 'withered', 'young',
-            ]
-    nouns = ['bird', 'breeze', 'brook', 'brook', 'bush',
-             'butterfly', 'chamber', 'chasm', 'cherry', 'cliff',
-             'cloud', 'darkness', 'dawn', 'dew', 'dream',
-             'dust', 'eye', 'feather', 'field', 'fire',
-             'firefly', 'flower', 'foam', 'fog', 'forest',
-             'frog', 'frost', 'glade', 'glitter', 'grass',
-             'hand', 'haze', 'hill', 'horizon', 'lake',
-             'leaf', 'lily', 'meadow', 'mist', 'moon',
-             'morning', 'mountain', 'night', 'paper', 'pebble',
-             'pine', 'planet', 'plateau', 'pond', 'rain',
-             'resonance', 'ridge', 'ring', 'river', 'sea',
-             'shadow', 'shape', 'silence', 'sky', 'smoke',
-             'snow', 'snowflake', 'sound', 'star', 'stream',
-             'sun', 'sun', 'sunset', 'surf', 'thunder',
-             'tome', 'tree', 'violet', 'voice', 'water',
-             'waterfall', 'wave', 'wave', 'wildflower', 'wind',
-             'wood',
-             ]
+
+    adjs = [
+      'autumn', 'hidden', 'bitter', 'misty', 'silent', 'empty', 'dry', 'dark',
+      'summer', 'icy', 'delicate', 'quiet', 'white', 'cool', 'spring', 'winter',
+      'patient', 'twilight', 'dawn', 'crimson', 'wispy', 'weathered', 'blue',
+      'billowing', 'broken', 'cold', 'damp', 'falling', 'frosty', 'green',
+      'long', 'late', 'lingering', 'bold', 'little', 'morning', 'muddy', 'old',
+      'red', 'rough', 'still', 'small', 'sparkling', 'throbbing', 'shy',
+      'wandering', 'withered', 'wild', 'black', 'young', 'holy', 'solitary',
+      'fragrant', 'aged', 'snowy', 'proud', 'floral', 'restless', 'divine',
+      'polished', 'ancient', 'purple', 'lively', 'nameless', 'lucky', 'odd', 'tiny',
+      'free', 'dry', 'yellow', 'orange', 'gentle', 'tight', 'super', 'royal', 'broad',
+      'steep', 'flat', 'square', 'round', 'mute', 'noisy', 'hushy', 'raspy', 'soft',
+      'shrill', 'rapid', 'sweet', 'curly', 'calm', 'jolly', 'fancy', 'plain', 'shinny'
+    ]
+
+    nouns = [
+      'waterfall', 'river', 'breeze', 'moon', 'rain', 'wind', 'sea', 'morning',
+      'snow', 'lake', 'sunset', 'pine', 'shadow', 'leaf', 'dawn', 'glitter',
+      'forest', 'hill', 'cloud', 'meadow', 'sun', 'glade', 'bird', 'brook',
+      'butterfly', 'bush', 'dew', 'dust', 'field', 'fire', 'flower', 'firefly',
+      'feather', 'grass', 'haze', 'mountain', 'night', 'pond', 'darkness',
+      'snowflake', 'silence', 'sound', 'sky', 'shape', 'surf', 'thunder',
+      'violet', 'water', 'wildflower', 'wave', 'water', 'resonance', 'sun',
+      'wood', 'dream', 'cherry', 'tree', 'fog', 'frost', 'voice', 'paper',
+      'frog', 'smoke', 'star', 'atom', 'band', 'bar', 'base', 'block', 'boat',
+      'term', 'credit', 'art', 'fashion', 'truth', 'disk', 'math', 'unit', 'cell',
+      'scene', 'heart', 'recipe', 'union', 'limit', 'bread', 'toast', 'bonus',
+      'lab', 'mud', 'mode', 'poetry', 'tooth', 'hall', 'king', 'queen', 'lion', 'tiger',
+      'penguin', 'kiwi', 'cake', 'mouse', 'rice', 'coke', 'hola', 'salad', 'hat'
+    ]
 
     if tokenHex:
-        tokenChars = "0123456789abcdef"
+        tokenChars = '0123456789abcdef'
 
     adj = choice(adjs)
     noun = choice(nouns)
